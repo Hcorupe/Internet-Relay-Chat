472b831c58836df89cfcd074843d45db01eab7a7
YuShuRChen <59946770+YuShuRChen@users.noreply.github.com>
2020-03-25 17:16:30 -0700

    Update ClientMain.java

--- src/sample/Client/ClientMain.java
+++ src/sample/Client/ClientMain.java
@@ -10,13 +10,9 @@ public class ClientMain extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("This one is first in the Client.");
         Parent root = FXMLLoader.load(getClass().getResource("ClientLogin.fxml"));
-        System.out.println("This one is first in the Client.");
         primaryStage.setTitle("Client Login");
-        System.out.println("This one is first in the Client.");
         primaryStage.setScene(new Scene(root));
-        System.out.println("This one is first in the Client.");
         primaryStage.show();
     }
     public static void main(String[] args){
